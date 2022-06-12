17dab9ffa7db9a69e5c5b5aac8b495c2667f54c8
Jewell Scott <jewelljrscott@gmail.com>
2022-06-12 18:11:23 -0400

    Fix scores

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ function App() {
   const [ twoScore, setTwoScore ] = useState(0);
   const [ threeScore, setThreeScore ] = useState(0);
 
+  const [ clueValue, setClueValue ] = useState(200);
+
   const [ wager, setWager ] = useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setWager(0);
   }
 
 
@@ -33,8 +36,8 @@ function App() {
             <p className="score">${oneScore}</p>
             <p>Contestant 1</p>
             <p>Signature</p>
-            <button onClick={() => setOneScore(oneScore - 200)}>- $VAL</button>
-            <button onClick={() => setOneScore(oneScore + 200)}>+ $VAL</button>
+            <button onClick={() => setOneScore(oneScore - parseInt(clueValue))}>- ${clueValue}</button>
+            <button onClick={() => setOneScore(oneScore + parseInt(clueValue))}>+ ${clueValue}</button>
           </div>
           <div className="score-card score-wager">
             <p className="score">${oneScore}</p>
